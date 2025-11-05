c8c7c49282121c28ed452b7b8ca4808c00ae7576
naswapuutri-bit <naswapuutri@gmail.com>
2025-11-05 13:16:30 +0700

    tambah 02 append

---
+++ 02.append/02.cxx
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string teks = "Halo";
+    teks.append(" Dunia");
+
+    cout << teks; // Output: Halo Dunia
+    return 0;
+}
