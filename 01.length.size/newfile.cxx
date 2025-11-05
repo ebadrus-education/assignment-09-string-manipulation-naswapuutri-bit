197629e18441c9a1e3199ee576da6208533b756e
naswapuutri-bit <naswapuutri@gmail.com>
2025-11-05 13:10:12 +0700

    tambah main cpp 01

---
+++ 01.length.size/newfile.cxx
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string teks = "Halo Dunia";
+    
+    cout << teks.length() << endl; // 10
+    cout << teks.size() << endl;   // 10
+    
+    return 0;
+}
